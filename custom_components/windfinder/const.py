0e16b36c8e2208cae916eb8401a1b0747c3dd618
Jasper Tonk <j.c.t.tonk@gmail.com>
2025-06-15 11:27:33 +0200

    Update const.py

--- custom_components/windfinder/const.py
+++ custom_components/windfinder/const.py
@@ -1,8 +1,8 @@
 DOMAIN = "windfinder"
-CONF_LOCATION = "location (has to be a valid WF location)"
+CONF_LOCATION = "location"
 FORECAST_URL = "https://www.windfinder.com/forecast/{}"
 SUPERFORECAST_URL = "https://www.windfinder.com/weatherforecast/{}"
 PLATFORMS = ["sensor", "button"]
 
-CONF_REFRESH_INTERVAL = "refresh_interval (minutes)"
+CONF_REFRESH_INTERVAL = "refresh_interval"
 DEFAULT_REFRESH_INTERVAL = 30  # minutes
